1824d35956edd0fbdaad985dab995f07959509be
Harounnn <medharoun.1919@gmail.com>
2024-03-14 01:38:33 +0100

    Adapter.java & Model.java added

---
+++ app/src/main/java/com/example/readitai/Adapter.java
@@ -0,0 +1,25 @@
+package com.example.readitai;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+//public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    //@NonNull
+    //@Override
+    //public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      //  return null;
+    //}
+
+  //  @Override
+    //public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    //}
+
+    //@Override
+    //public int getItemCount() {
+      //  return 0;
+    //}
+//}
---
+++ app/src/main/java/com/example/readitai/Model.java
@@ -0,0 +1,8 @@
+package com.example.readitai;
+
+public class Model {
+
+    private String sourceName,title,url,urlToImage,content;
+
+
+}
