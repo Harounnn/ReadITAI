b0957bbe23ef103da035e81aa2f51847298c49f9
Harounnn <medharoun.1919@gmail.com>
2024-03-16 01:01:25 +0100

    sumNews.java added + Adapter.java & layout_item.xml modified

--- app/src/main/java/com/example/readitai/Adapter.java
+++ app/src/main/java/com/example/readitai/Adapter.java
@@ -1,13 +1,18 @@
 package com.example.readitai;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -24,7 +29,18 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, sumNews.class);
+                intent.putExtra("content", models.get(position).getContent());
+                context.startActivity(intent);
+            }
+        });
 
+        holder.heading.setText(models.get(position).getTitle());
+        holder.description.setText(models.get(position).getContent());
+        Glide.with(context).load(models.get(position).getUrlToImage()).into(holder.image);
     }
 
     @Override
@@ -34,9 +50,15 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView heading,content;
+        TextView heading,description;
+        CardView cardView;
+        ImageView image;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            heading = itemView.findViewById(R.id.heading);
+            description = itemView.findViewById(R.id.description);
+            cardView = itemView.findViewById(R.id.cardView);
+            image = itemView.findViewById(R.id.image);
         }
     }
 }
