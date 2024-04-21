add2205bc04bcd3274da7905a05b70dfc2fe70c2
Harounnn <medharoun.1919@gmail.com>
2024-04-21 15:32:56 +0100

    Summarizatipn API removed and color code changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.github.RadimRehurek:Gensim:4.1.0")
 }
