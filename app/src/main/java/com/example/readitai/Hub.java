4420cf10afc0234041459a528e5f32f0afe0f8d5
Harounnn <medharoun.1919@gmail.com>
2024-04-23 22:38:22 +0100

    Added all activity-related xml and java files

---
+++ app/src/main/java/com/example/readitai/Hub.java
@@ -0,0 +1,14 @@
+package com.example.readitai;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Hub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hub);
+    }
+}
