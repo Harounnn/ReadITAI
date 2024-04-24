b6fd35caec22581fef2fca0822d9e1f825443e7b
Harounnn <medharoun.1919@gmail.com>
2024-04-24 20:36:14 +0100

    main colors added to colors.xml

--- app/src/main/java/com/example/readitai/AccountFragment.java
+++ app/src/main/java/com/example/readitai/AccountFragment.java
@@ -1,6 +1,24 @@
 package com.example.readitai;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
 
 public class AccountFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.account_fragment, null);
+
+        return v;
+    }
 }
--- app/src/main/java/com/example/readitai/FavouritesFragment.java
+++ app/src/main/java/com/example/readitai/FavouritesFragment.java
@@ -1,6 +1,21 @@
 package com.example.readitai;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FavouritesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.favourites_fragment, null);
+
+        return v;
+    }
 }
