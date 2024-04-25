61c62291571ded821ff6966228cd421f898ed60f
Harounnn <medharoun.1919@gmail.com>
2024-04-25 18:10:22 +0100

    summarization.xml modified

--- app/src/main/java/com/example/readitai/Hub.java
+++ app/src/main/java/com/example/readitai/Hub.java
@@ -1,6 +1,9 @@
 package com.example.readitai;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class Hub extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hub);
+
+        Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Hub.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
