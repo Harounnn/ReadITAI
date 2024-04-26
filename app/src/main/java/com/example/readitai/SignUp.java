69bf56952c1f99c3c5f88cf3fb5a734e1a827607
Harounnn <medharoun.1919@gmail.com>
2024-04-26 18:10:35 +0100

    Sign Up done

--- app/src/main/java/com/example/readitai/SignUp.java
+++ app/src/main/java/com/example/readitai/SignUp.java
@@ -1,16 +1,24 @@
 package com.example.readitai;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    Validator validator = new Validator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,13 +29,35 @@ public class SignUp extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         EditText email = (EditText) findViewById(R.id.usernamesu);
-        String usernameText = String.valueOf(email.getText());
+        String emailText = String.valueOf(email.getText());
 
         EditText password = (EditText) findViewById(R.id.passwordsu);
         String passwordText = String.valueOf(password.getText());
 
         EditText passwordAgain = (EditText) findViewById(R.id.passwordAgain);
         String passwordAgainText = String.valueOf(passwordAgain.getText());
+
+        Button signUpButton = (Button) findViewById(R.id.signUpButtonTwo);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validator.isValidEmail(emailText) && validator.isValidPassword(passwordText) && passwordText == passwordAgainText){
+                    mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                    } else {
+                                        Toast.makeText(SignUp.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/readitai/Validator.java
+++ app/src/main/java/com/example/readitai/Validator.java
@@ -6,19 +6,18 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-    // Regular expression patterns for email and password validation
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{7,}$";
 
     private static final Pattern emailPattern = Pattern.compile(EMAIL_PATTERN);
     private static final Pattern passwordPattern = Pattern.compile(PASSWORD_PATTERN);
 
-    public static boolean isValidEmail(String email) {
+    public boolean isValidEmail(String email) {
         Matcher matcher = emailPattern.matcher(email);
         return matcher.matches();
     }
 
-    public static boolean isValidPassword(String password) {
+    public boolean isValidPassword(String password) {
         Matcher matcher = passwordPattern.matcher(password);
         return matcher.matches();
     }
